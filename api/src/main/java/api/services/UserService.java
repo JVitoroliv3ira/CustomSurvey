c8403067289a4ccd3045f39bdbf3cf9d060c8d69
João Oliveira <jvitoroliveira0711@gmail.com>
2024-03-10 16:27:19 -0300

    feature(#1:backend): Implementa metodo de cadastro de usuario

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,11 +1,41 @@
 package api.services;
 
+import api.exceptions.ApiException;
+import api.helpers.EncoderHelper;
+import api.models.User;
 import api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final EncoderHelper encoderHelper;
+
+    public User register(User entity) {
+        this.validateEmailUniqueness(entity.getEmail());
+        this.encodeUserPassword(entity);
+        return this.create(entity);
+    }
+
+    private void validateEmailUniqueness(String email) {
+        if (this.userRepository.existsByEmailIgnoreCase(email)) {
+            throw new ApiException(
+                    "O email informado está sendo utilizado por outra conta.",
+                    "email",
+                    HttpStatus.BAD_REQUEST
+            );
+        }
+    }
+
+    private void encodeUserPassword(User entity) {
+        entity.setPassword(this.encoderHelper.encode(entity.getPassword()));
+    }
+
+    private User create(User entity) {
+        entity.setId(null);
+        return this.userRepository.save(entity);
+    }
 }
